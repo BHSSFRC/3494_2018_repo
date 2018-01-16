b59b81e9c26ae622560bb28eee63e4b5c7c7c1ca
Edwan Vi <edwan.vi@gmail.com>
2018-01-16 16:13:59 -0500

    Implement a basic vision test in auton

--- src/main/java/org/usfirst/frc/team3494/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team3494/robot/Robot.java
@@ -1,10 +1,14 @@
 package org.usfirst.frc.team3494.robot;
 
 import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team3494.robot.subsystems.Drivetrain;
+import org.usfirst.frc.team3494.robot.util.Limelight;
 
 
 public class Robot extends IterativeRobot {
@@ -14,8 +18,12 @@ public class Robot extends IterativeRobot {
      * can read button values from it.
      */
     public static OI oi;
-    public static Drivetrain driveTrain;
+
     public static AHRS ahrs;
+    public static Limelight limelight;
+    UsbCamera camera_0;
+
+    public static Drivetrain driveTrain;
 
     /**
      * This function is run when the robot is first started up and should be
@@ -25,12 +33,42 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
         oi = new OI();
         ahrs = new AHRS(SPI.Port.kMXP);
+        limelight = new Limelight();
 
         driveTrain = new Drivetrain();
+
+        SmartDashboard.putNumber("Limelight pipeline", 0);
+        camera_0 = CameraServer.getInstance().startAutomaticCapture("flaming bagpipes", 0);
     }
 
     @Override
     public void autonomousInit() {
         fieldData = DriverStation.getInstance().getGameSpecificMessage();
+        limelight.setLEDs(Limelight.LIMELIGHT_LED_ON);
+        int pipe = (int) SmartDashboard.getNumber("Limelight pipeline", 0);
+        if (pipe == 1) {
+            limelight.setLEDs(Limelight.LIMELIGHT_LED_OFF);
+        }
+        limelight.setPipeline(pipe);
+        camera_0.setExposureManual(20);
+        Robot.driveTrain.enable();
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        Robot.driveTrain.ArcadeDrive(0.65, Robot.driveTrain.pidTune, true);
+        double tx = limelight.getXDistance();
+        Robot.driveTrain.setSetpoint(Robot.ahrs.getYaw() + tx);
+        SmartDashboard.putNumber("Drive PID setpoint", Robot.driveTrain.getSetpoint());
+    }
+
+    @Override
+    public void teleopInit() {
+        limelight.setLEDs(Limelight.LIMELIGHT_LED_OFF);
+    }
+
+    @Override
+    public void disabledInit() {
+        limelight.setLEDs(Limelight.LIMELIGHT_LED_OFF);
     }
 }
