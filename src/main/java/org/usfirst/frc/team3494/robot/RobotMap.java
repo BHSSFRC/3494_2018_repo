907ec05e0cd0b7d669b2122f0c40604b6e6ac1f3
Evie/Katrina 3494 <butterfly5748@gmail.com>
2018-01-16 19:26:31 -0500

    the programming padawans created a roller claw program

--- src/main/java/org/usfirst/frc/team3494/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team3494/robot/RobotMap.java
@@ -7,6 +7,8 @@ package org.usfirst.frc.team3494.robot;
  * floating around.
  */
 public class RobotMap {
+    public static final int ROLLER_LEFT = 7;
+    public static final int ROLLER_RIGHT = 8;
     // For example to map the left and right motors, you could define the
     // following variables to use with your driveTrain subsystem.
     // public static int leftMotor = 1;
