a964ec3f385cfef731a501e5d3171d5435936a36
Edwan Vi <edwan.vi@gmail.com>
2018-02-01 16:36:00 -0500

    Remove EncoderFollowers

--- src/main/java/org/usfirst/frc/team3494/robot/commands/auto/tests/PathTestOne.java
+++ src/main/java/org/usfirst/frc/team3494/robot/commands/auto/tests/PathTestOne.java
@@ -11,13 +11,11 @@ import org.usfirst.frc.team3494.robot.RobotMap;
 
 public class PathTestOne extends Command {
 
-    private EncoderFollower left;
-    private EncoderFollower right;
-
     private Trajectory leftTraj;
     private Trajectory rightTraj;
 
     private double startTime;
+    private int index;
 
     public PathTestOne() {
         requires(Robot.driveTrain);
@@ -32,40 +30,21 @@ public class PathTestOne extends Command {
         leftTraj = modifier.getLeftTrajectory();
         rightTraj = modifier.getRightTrajectory();
 
-        left = new EncoderFollower(modifier.getLeftTrajectory());
-        left.configureEncoder(Robot.driveTrain.getCountsLeft_Talon(), 256 * 4, 4.875);
-        left.configurePIDVA(0.2, 0.0, 0.0, 1 / RobotMap.PATH_MAX_SPEED, 0);
-
-        right = new EncoderFollower(modifier.getRightTrajectory());
-        right.configureEncoder(Robot.driveTrain.getCountsRight_Talon(), 256 * 4, 4.875);
-        right.configurePIDVA(0.2, 0.0, 0.0, 1 / RobotMap.PATH_MAX_SPEED, 0);
-
         startTime = Timer.getFPGATimestamp() * 1000.0;
     }
 
     @Override
     protected void execute() {
-        int index = ((int) Math.floor(((Timer.getFPGATimestamp() * 1000.0) - startTime) / 50));
+        index = ((int) Math.floor(((Timer.getFPGATimestamp() * 1000.0) - startTime) / 50));
 
         double leftVelo = leftTraj.segments[index].velocity;
         double rightVelo = rightTraj.segments[index].velocity;
-
-        double leftVal = left.calculate(Robot.driveTrain.getCountsLeft_Talon());
-        double rightVal = right.calculate(Robot.driveTrain.getCountsRight_Talon());
-
-        double gyro_heading = Robot.ahrs.getAngle();    // Assuming the gyro is giving a value in degrees
-        double desired_heading = Pathfinder.r2d(left.getHeading());  // Should also be in degrees
-
-        double angleDifference = Pathfinder.boundHalfDegrees(desired_heading - gyro_heading);
-        double turn = 0.8 * (-1.0 / 80.0) * angleDifference;
-
-        // Robot.driveTrain.TankDrive(leftVal + turn, rightVal - turn);
-        Robot.driveTrain.VelocityTank(metersToCounts(leftVelo), metersToCounts(rightVelo));
+        Robot.driveTrain.VelocityTank(metersToCounts(leftVelo) / 10, metersToCounts(rightVelo) / 10);
     }
 
     @Override
     protected boolean isFinished() {
-        return left.isFinished();
+        return index + 1 >= leftTraj.segments.length;
     }
 
     @Override
