1b7c1ff019bb9b5330851078efaa2311ca3040c2
Edwan Vi <edwan.vi@gmail.com>
2018-02-07 19:47:43 -0500

    Create auto command for driving by passed-in files

---
+++ src/main/java/org/usfirst/frc/team3494/robot/commands/auto/ProfileFollower.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team3494.robot.commands.auto;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import org.usfirst.frc.team3494.robot.Robot;
+
+import java.io.File;
+
+public class ProfileFollower extends Command {
+
+    private Trajectory left;
+    private Trajectory right;
+
+    private double startTime;
+    private int index;
+
+    public ProfileFollower(String leftFile, String rightFile) {
+        requires(Robot.driveTrain);
+        left = Pathfinder.readFromCSV(new File(leftFile));
+        right = Pathfinder.readFromCSV(new File(rightFile));
+    }
+
+    @Override
+    protected void initialize() {
+        Robot.driveTrain.resetEncoders();
+        startTime = Timer.getFPGATimestamp() * 1000.0;
+    }
+
+    @Override
+    protected void execute() {
+        index = ((int) Math.floor(((Timer.getFPGATimestamp() * 1000.0) - startTime) / 50));
+
+        double leftVelo = left.segments[index].velocity;
+        double rightVelo = right.segments[index].velocity;
+        Robot.driveTrain.VelocityTank(
+                (Robot.feetToEdges(leftVelo)) / 10,
+                (Robot.feetToEdges(rightVelo)) / 10);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return index + 1 >= left.segments.length;
+    }
+}
