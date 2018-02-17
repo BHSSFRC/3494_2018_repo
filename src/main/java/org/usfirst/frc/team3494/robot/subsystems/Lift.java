52cdc67c2c4f2b4713f0612175bcc2924836a821
Edwan Vi <edwan.vi@gmail.com>
2018-02-16 20:59:46 -0500

    Add Lift docs.

--- src/main/java/org/usfirst/frc/team3494/robot/subsystems/Lift.java
+++ src/main/java/org/usfirst/frc/team3494/robot/subsystems/Lift.java
@@ -5,7 +5,13 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team3494.robot.RobotMap;
 
+/**
+ * The lift subsystem. Contains methods for controlling the robot's lift.
+ */
 public class Lift extends Subsystem {
+    /**
+     * The single motor that runs the lift up and down.
+     */
     private TalonSRX liftMotor;
 
     public Lift() {
@@ -16,6 +22,12 @@ public class Lift extends Subsystem {
     protected void initDefaultCommand() {
     }
 
+    /**
+     * Runs the lift motor at the specified power.
+     *
+     * @param power The power to run the lift at. Should be a {@code double}
+     *              between -1 and 1.
+     */
     public void lift(double power) {
         liftMotor.set(ControlMode.PercentOutput, power);
     }
