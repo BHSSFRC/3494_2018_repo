a57a7cd28198ad9628903bb0d85a7f33492d6312
Edwan Vi <edwan.vi@gmail.com>
2018-02-20 17:25:13 -0500

    Add shebang

--- src/paths/upload_all.sh
+++ src/paths/upload_all.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo "Pinging target ($1)..."
 ping -c 4 $1 -q
 echo "Uploading center csv files to $1"
@@ -6,4 +7,3 @@ echo "Uploading left csv files to $1"
 scp -r ./left lvuser@$1:/home/lvuser/paths
 echo "Uploading right csv files to $1"
 scp -r ./right lvuser@$1:/home/lvuser/paths
-
