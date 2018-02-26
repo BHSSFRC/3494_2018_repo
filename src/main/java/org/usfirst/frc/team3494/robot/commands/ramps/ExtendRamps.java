4bbcf63734d0001ee40d54dc439089fd8b287292
Edwan Vi <edwan.vi@gmail.com>
2018-02-26 16:03:20 -0500

    Move ExtendRamps to new package

--- src/main/java/org/usfirst/frc/team3494/robot/commands/ramps/ExtendRamps.java
+++ src/main/java/org/usfirst/frc/team3494/robot/commands/ramps/ExtendRamps.java
@@ -1,4 +1,4 @@
-package org.usfirst.frc.team3494.robot.commands;
+package org.usfirst.frc.team3494.robot.commands.ramps;
 
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team3494.robot.Robot;
