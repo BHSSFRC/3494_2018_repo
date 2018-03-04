5b606a08ef07b7786bc2956abea6aabd25c35b4c
Edwan Vi <edwan.vi@gmail.com>
2018-03-04 10:10:27 -0500

    Remove cube if on "hot" side

--- src/main/java/org/usfirst/frc/team3494/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team3494/robot/Robot.java
@@ -161,7 +161,11 @@ public class Robot extends IterativeRobot {
             autoCmd.start();
         } else {
             if (String.valueOf(fieldData.charAt(0)).equals(positionChooser.getSelected())) {
-                autoCmd = new DistanceDrive(10.0D - (33.0 / 12.0), true);
+                Command[] list = new Command[]{
+                        new DistanceDrive(10.0D - (33.0 / 12.0), true),
+                        new RemoveCube()
+                };
+                autoCmd = new DynamicAutoCommand(list);
             } else {
                 autoCmd = new DistanceDrive(10.0D - (30.0 / 12.0), false);
             }
