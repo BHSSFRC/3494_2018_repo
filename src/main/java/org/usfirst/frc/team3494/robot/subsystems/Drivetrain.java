0bb4a09aac868449ab8ef2ec51d72d3d0ef9326e
Edwan Vi <edwan.vi@gmail.com>
2018-03-09 21:57:17 -0500

    Make talon PID more agressive

--- src/main/java/org/usfirst/frc/team3494/robot/subsystems/Drivetrain.java
+++ src/main/java/org/usfirst/frc/team3494/robot/subsystems/Drivetrain.java
@@ -53,7 +53,7 @@ public class Drivetrain extends PIDSubsystem {
 
     public Drivetrain() {
         super("Drivetrain", 0.025, 0, 0);
-        double talon_P = 1.6;
+        double talon_P = 3.0D;
 
         this.driveLeftMaster = new TalonSRX(RobotMap.DRIVE_LEFT_MASTER);
         this.driveLeftMaster.setNeutralMode(NeutralMode.Brake);
