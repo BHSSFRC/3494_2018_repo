f5f32cb093ff15a164481642da4c0a29900db4f3
Edwan Vi <edwan.vi@gmail.com>
2018-03-27 21:06:03 -0400

    Remove POV control from Lift command

--- src/main/java/org/usfirst/frc/team3494/robot/commands/lift/Lift.java
+++ src/main/java/org/usfirst/frc/team3494/robot/commands/lift/Lift.java
@@ -5,36 +5,17 @@ import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team3494.robot.Robot;
 
 public class Lift extends Command {
-
-    private static final double TOP_POSITION = Double.MAX_VALUE;
-    private static final double EXCHANGE_POSITION = org.usfirst.frc.team3494.robot.subsystems.Lift.revsToCounts(org.usfirst.frc.team3494.robot.subsystems.Lift.inchesToRevs(2.0D));
-    private static final double BOTTOM_POSITION = 0;
-
     public Lift() {
         requires(Robot.lift);
     }
 
     @Override
     protected void execute() {
-        int pov = Robot.oi.getXbox().getPOV();
         double stick = Robot.applyDeadband(-Robot.oi.getXbox().getY(GenericHID.Hand.kLeft), 0.01);
-        if (stick != 0) {
-            if (stick < 0) {
-                Robot.lift.lift(stick / 2);
-            } else {
-                Robot.lift.lift(stick / 1.6);
-            }
-        }
-        if (pov == 180) {
-            if (Robot.lift.getHeight_Edges() != 0) {
-                if (Robot.lift.getHeight_Edges() > 0 && !Robot.lift.getHallBottom()) {
-                    Robot.lift.lift(-0.4);
-                } else {
-                    Robot.lift.lift(0.4);
-                }
-            } else {
-                Robot.lift.lift(0);
-            }
+        if (stick < 0) {
+            Robot.lift.lift(stick / 2);
+        } else {
+            Robot.lift.lift(stick / 1.6);
         }
     }
 
