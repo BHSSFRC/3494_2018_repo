f58110e15b435d18b82d5a291ba97d0021d2827a
Caleb Berger <caleb.x.berger@gmail.com>
2018-10-18 16:55:55 -0400

    Use continuous current limit instead of peak Peak current limiting was either broken or needed additional configuration. This works around that problem.

--- src/main/java/org/usfirst/frc/team3494/robot/subsystems/Drivetrain.java
+++ src/main/java/org/usfirst/frc/team3494/robot/subsystems/Drivetrain.java
@@ -68,7 +68,7 @@ public class Drivetrain extends PIDSubsystem {
         this.driveLeftMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
         this.driveLeftMaster.config_kP(0, talon_P, 10);
         this.driveLeftMaster.config_kF(0, 1023 / (RobotMap.PATH_MAX_SPEED * RobotMap.COUNTS_PER_METER * 4.0 * (1.0 / 10.0)), 10);
-        this.driveLeftMaster.configPeakCurrentLimit(40, 10);
+        this.driveLeftMaster.configContinuousCurrentLimit(40, 10);
         this.driveLeftMaster.enableCurrentLimit(true);
 
         this.driveLeftMaster.clearMotionProfileTrajectories();
@@ -88,7 +88,7 @@ public class Drivetrain extends PIDSubsystem {
         this.driveRightMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
         this.driveRightMaster.config_kP(0, talon_P, 10);
         this.driveRightMaster.config_kF(0, 1023 / (RobotMap.PATH_MAX_SPEED * RobotMap.COUNTS_PER_METER * 4.0 * (1.0 / 10.0)), 10);
-        this.driveRightMaster.configPeakCurrentLimit(40, 10);
+        this.driveRightMaster.configContinuousCurrentLimit(40, 10);
         this.driveRightMaster.enableCurrentLimit(true);
         this.driveRightMaster.setInverted(true);
         this.driveRightMaster.setSensorPhase(true);
